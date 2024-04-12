fd9a4a463fbe61930f8e88667c37459c6b61496a
Hiroki Yamada <yamada4tec@gmail.com>
2024-04-12 19:32:11 +0900

    Update: teleopkey program to support all products.

--- interactive-customer-service-ros/src/teleop_key_ics.cpp
+++ interactive-customer-service-ros/src/teleop_key_ics.cpp
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <signal.h>
-#include <termios.h>
 #include <ros/ros.h>
-#include <std_msgs/String.h>
 #include <sensor_msgs/Image.h>
 #include <interactive_customer_service/Conversation.h>
 #include <interactive_customer_service/RobotStatus.h>
@@ -15,49 +13,10 @@ using namespace cv;
 class InteractiveCustomerServiceTeleopKey
 {
 private:
-  static const char KEYCODE_0 = 0x30;
-  static const char KEYCODE_1 = 0x31;
-  static const char KEYCODE_2 = 0x32;
-  static const char KEYCODE_3 = 0x33;
-  static const char KEYCODE_4 = 0x34;
-  static const char KEYCODE_5 = 0x35;
-  static const char KEYCODE_6 = 0x36;
-  static const char KEYCODE_7 = 0x37;
-  static const char KEYCODE_8 = 0x38;
-  static const char KEYCODE_9 = 0x39;
-
   const std::string MSG_ARE_YOU_READY = "Are_you_ready?";
-
   const std::string MSG_I_AM_READY    = "I_am_ready";
-  const std::string MSG_ROBOT_MESSAGE = "robot_message";
-  const std::string MSG_TAKE_ITEM     = "take_item";
-  const std::string MSG_GIVE_ITEM     = "give_item";
-  const std::string MSG_GIVE_UP       = "Give_up";
-  
-  const std::string OBJ1 = "pie_no_mi-1000";
-  const std::string OBJ2 = "11_xylitol-1000";
-  const std::string OBJ3 = "chipstar_consomme-2000";
-  const std::string OBJ4 = "irohasu-3000";
-  const std::string OBJ5 = "donbee_soba";
   
   ros::Publisher pub_msg_;
-  
-  void showHelp()
-  {
-    puts("Operate by Keyboard");
-    puts("---------------------------");
-//    puts(("0 : Send "+MSG_I_AM_READY).c_str());
-    puts( "1 : Send Message : 'There are several candidates. Is it green?'");
-    puts( "2 : Send Message : 'Is this what you want?'");
-    puts(("3 : Take "+OBJ1).c_str());
-    puts(("4 : Take "+OBJ2).c_str());
-    puts(("5 : Take "+OBJ3).c_str());
-    puts(("6 : Take "+OBJ4).c_str());
-    puts(("7 : Take "+OBJ5).c_str());
-    puts( "8 : Give Item");
-    puts(("9 : Send "+MSG_GIVE_UP).c_str());
-    puts("---------------------------");
-  }
 
   void messageCallback(const interactive_customer_service::Conversation::ConstPtr& message)
   {
@@ -70,7 +29,7 @@ private:
       ROS_INFO("Subscribe message:%s, %s", message->type.c_str(), message->detail.c_str());
     }
   }
-
+  
   void robotStatusCallback(const interactive_customer_service::RobotStatus::ConstPtr& status)
   {
     ROS_DEBUG("Subscribe robot status:%s, %d, %s", status->state.c_str(), status->speaking, status->grasped_item.c_str());
@@ -117,42 +76,10 @@ private:
     ros::shutdown();
   }
 
-  static int canReceive(int fd)
-  {
-    fd_set fdset;
-    int ret;
-    struct timeval timeout;
-    FD_ZERO( &fdset );
-    FD_SET( fd , &fdset );
-
-    timeout.tv_sec = 0;
-    timeout.tv_usec = 0;
-
-    return select( fd+1 , &fdset , NULL , NULL , &timeout );
-  }
-
 public:
   int run(int argc, char **argv)
   {
     ros::NodeHandle node_handle;
-
-    char c;
-
-    /////////////////////////////////////////////
-    // get the console in raw mode
-    int kfd = 0;
-    struct termios cooked;
-
-    struct termios raw;
-    tcgetattr(kfd, &cooked);
-    memcpy(&raw, &cooked, sizeof(struct termios));
-    raw.c_lflag &=~ (ICANON | ECHO);
-    raw.c_cc[VEOL] = 1;
-    raw.c_cc[VEOF] = 2;
-    tcsetattr(kfd, TCSANOW, &raw);
-    /////////////////////////////////////////////
-
-    showHelp();
 
     // Override the default ros sigint handler.
     // This must be set after the first NodeHandle is created.
@@ -160,84 +87,13 @@ public:
   
     ros::Rate loop_rate(10);
 
-    ros::Time waiting_start_time;
-
     ros::Subscriber sub_msg   = node_handle.subscribe<interactive_customer_service::Conversation>("/interactive_customer_service/message/customer", 100, &InteractiveCustomerServiceTeleopKey::messageCallback, this);
     ros::Subscriber sub_state = node_handle.subscribe<interactive_customer_service::RobotStatus >("/interactive_customer_service/robot_status",     100, &InteractiveCustomerServiceTeleopKey::robotStatusCallback, this);
     ros::Subscriber sub_image = node_handle.subscribe<sensor_msgs::Image>                        ("/interactive_customer_service/customer_image",   100, &InteractiveCustomerServiceTeleopKey::customerImageCallback, this);
 
     pub_msg_ = node_handle.advertise<interactive_customer_service::Conversation>("/interactive_customer_service/message/robot", 10);
 
-    while (ros::ok())
-    {
-      if(canReceive(kfd))
-      {
-        // get the next event from the keyboard
-        if(read(kfd, &c, 1) < 0)
-        {
-          perror("read():");
-          exit(EXIT_FAILURE);
-        }
-
-        switch(c)
-        {
-          case KEYCODE_1:
-          {
-            sendMessage(pub_msg_, MSG_ROBOT_MESSAGE, "There are several candidates. Is it green?");
-            break;
-          }
-          case KEYCODE_2:
-          {
-            sendMessage(pub_msg_, MSG_ROBOT_MESSAGE, "Is this what you want?");
-            break;
-          }
-          case KEYCODE_3:
-          {
-            sendMessage(pub_msg_, MSG_TAKE_ITEM, OBJ1);
-            break;
-          }
-          case KEYCODE_4:
-          {
-            sendMessage(pub_msg_, MSG_TAKE_ITEM, OBJ2);
-            break;
-          }
-          case KEYCODE_5:
-          {
-            sendMessage(pub_msg_, MSG_TAKE_ITEM, OBJ3);
-            break;
-          }
-          case KEYCODE_6:
-          {
-            sendMessage(pub_msg_, MSG_TAKE_ITEM, OBJ4);
-            break;
-          }
-          case KEYCODE_7:
-          {
-            sendMessage(pub_msg_, MSG_TAKE_ITEM, OBJ5);
-            break;
-          }
-          case KEYCODE_8:
-          {
-            sendMessage(pub_msg_, MSG_GIVE_ITEM);
-            break;
-          }
-          case KEYCODE_9:
-          {
-            sendMessage(pub_msg_, MSG_GIVE_UP);
-            break;
-          }
-        }
-      }
-
-      ros::spinOnce();
-
-      loop_rate.sleep();
-    }
-
-    /////////////////////////////////////////////
-    // cooked mode
-    tcsetattr(kfd, TCSANOW, &cooked);
-    /////////////////////////////////////////////
+    ros::spin();
 
     return EXIT_SUCCESS;
   }
